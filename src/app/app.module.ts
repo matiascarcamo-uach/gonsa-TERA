ce2ae9933d3418ca9423eccdd63ca69112afdf2e
Jorge Obando <sebita_118@hotmail.com>
2019-01-12 03:07:54 +0100

    tarea y archivos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,26 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 
-import { ROUTES } from './app.routes'
+import { ROUTES } from './app.routes';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { HttpClientModule } from '@angular/common/http';
 import { EstudianteComponent } from './components/estudiante/estudiante.component';
+import { ProfesorComponent } from './components/profesor/profesor.component';
+
+import { DesafioComponent } from './components/desafios/desafios.component';
+import { TareasComponent } from './components/tareas/tareas.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     LoginComponent,
-    EstudianteComponent
+    EstudianteComponent,
+    ProfesorComponent,
+    DesafioComponent,
+    TareasComponent
   ],
   imports: [
     BrowserModule,
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,16 @@ import { LoginComponent } from "./components/login/login.component";
 import { Routes } from '@angular/router';
 import { LoginGuard } from './guards/login.guard';
 import { EstudianteComponent } from './components/estudiante/estudiante.component';
+import { ProfesorComponent } from './components/profesor/profesor.component';
+import { TareasComponent } from './components/tareas/tareas.component';
+import { DesafioComponent } from './components/desafios/desafios.component';
 
 export const ROUTES: Routes = [
     { path: 'home', component: HomeComponent, },
     { path: 'login', component: LoginComponent },
+    {path: 'profesor', component: ProfesorComponent},
+    {path: 'tareas', component: TareasComponent},
+    {path: 'desafio', component: DesafioComponent},
     { path: 'estudiante', component: EstudianteComponent, canActivate: [LoginGuard] },
     { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ]
